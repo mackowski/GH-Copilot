b3e10d80df6e0e7756f8cdbd0a318806d1665c6c
mackowski <35339942+mackowski@users.noreply.github.com>
2024-04-29 15:13:09 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -46,8 +46,5 @@ export async function run(): Promise<void> {
         body: ai_response
       })
     }
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
-  }
+  } 
 }
